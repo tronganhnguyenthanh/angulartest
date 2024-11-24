807ae568ecc49fa45a0749f568a2f7b05b955c48
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-11-24 14:08:58 +0700

    [UPDATE][BE] build import api

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv")
 dotenv.config()
 const cors = require("cors")
 const mongoose = require("mongoose")
+const multer  = require('multer')
+const upload = multer({dest:'uploads/'})
 const routerAPI = require("./routes/routes.api")
 mongoose.connect(process.env.DB).then(() => {console.log("Database connect successfully")}).catch(() => {
  console.log("Failed to connect")
@@ -14,3 +16,11 @@ app.listen(process.env.PORT, () => {
 app.use(express.json())
 app.use(cors())
 app.use("/api", routerAPI)
+app.post("/file/import", upload.single("file"), function(req, res){
+  try{
+    let file = req.file
+    res.status(200).json(file)
+  }catch(error){
+    res.status(400).json({message:error})
+  }
+})
